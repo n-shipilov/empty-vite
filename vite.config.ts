142df035f7a09dab82d15652c7c650d0638d23f3
nshipilov <nshiplov@softailor.ru>
2025-03-26 12:07:38 +0300

    update packages

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,14 @@ import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    open: true,
-    port: 5173,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern-compiler",
-      },
-    },
-  },
+  server: { open: true, port: 5173 },
+  css: { preprocessorOptions: { scss: { api: "modern-compiler" } } },
   plugins: [
     react(),
     checker({
-      typescript: {
-        tsconfigPath: "tsconfig.json",
-      },
-      stylelint: {
-        lintCommand: "stylelint ./src/**/*.{scss}",
-      },
-      eslint: {
-        useFlatConfig: true,
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-      },
+      typescript: { tsconfigPath: "tsconfig.json" },
+      stylelint: { lintCommand: "stylelint ./src/**/*.{scss}" },
+      eslint: { useFlatConfig: true, lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
     }),
   ],
 });
